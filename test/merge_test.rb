7365255a8c614d10fb0ab99506be4d17725e6736
Dirk Geurs <dirklectisch@mac.com>
2011-04-15 11:55:12 +0200

    + Unit test for merging EndPoint and request params

---
+++ test/merge_test.rb
@@ -0,0 +1,23 @@
+
+require File.dirname(__FILE__) + '/base.rb'
+
+
+class MergeTest < Test::Unit::TestCase
+
+  include Rufus::Verbs
+
+
+  def test_0
+    
+    ep = EndPoint.new(
+      :dry_run => true,
+      :uri => 'http://host/path',
+      :params => { 'token' => 'apiaccess'})
+
+    req = ep.get(
+      :params => { 'extra' => 'param'})
+
+    assert_equal "/path?token=apiaccess&extra=param", req.path
+    
+  end
+end
